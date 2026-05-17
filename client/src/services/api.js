9729ed6575d777f9ecb4082026eea2fa99338b8a
chandan <chan190202@gmail.com>
2026-05-17 21:00:30 +0530

    add backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
+console.log("API Base URL:", import.meta.env.VITE_API_URL);
 // Attach JWT token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
